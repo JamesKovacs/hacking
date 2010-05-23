3596a98d3829513e526e5549f631b76692c5c405
James Kovacs <jkovacs@post.harvard.edu>
2010-05-22 19:11:30 -0600

    Added solution to problem #004.

---
+++ ProjectEuler/ProjectEuler/Problem004.fs
@@ -0,0 +1,19 @@
+﻿module Problem004
+
+(* #004: 
+    A palindromic number reads the same both ways. The largest palindrome made 
+    from the product of two 2-digit numbers is 9009 = 91 × 99.
+    Find the largest palindrome made from the product of two 3-digit numbers.
+*)
+let Run =
+    let isPalindrome n =
+        let s = sprintf "%d" n |> Array.ofSeq
+        let rev_s = Array.rev s
+        Array.forall2 (fun a b -> a = b) s rev_s
+
+    let threeDigits = {999..-1..100}
+    let products = Seq.collect (fun x -> Seq.map (fun y -> x*y) threeDigits) threeDigits
+    let largestPalindrome = products |> Seq.filter isPalindrome |> Seq.max
+    printfn "#004: %d" largestPalindrome
+    assert (largestPalindrome = 906609)
+    

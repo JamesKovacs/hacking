128fc3e267562e98de85e3cba78689cfcbf16233
James Kovacs <jkovacs@post.harvard.edu>
2010-05-22 20:54:01 -0600

    Improved performance of solution #004.

--- ProjectEuler/ProjectEuler/Problem004.fs
+++ ProjectEuler/ProjectEuler/Problem004.fs
@@ -6,13 +6,17 @@
     Find the largest palindrome made from the product of two 3-digit numbers.
 *)
 let Run =
+    let reverse n =
+        let rec loop n res =
+            if n = 0 then res
+            else loop (n/10) (res*10 + n%10)
+        loop n 0
+
     let isPalindrome n =
-        let s = sprintf "%d" n |> Array.ofSeq
-        let rev_s = Array.rev s
-        Array.forall2 (fun a b -> a = b) s rev_s
+        n = reverse n
 
     let threeDigits = {999..-1..100}
-    let products = Seq.collect (fun x -> Seq.map (fun y -> x*y) threeDigits) threeDigits
+    let products = Seq.collect (fun x -> Seq.map (fun y -> x*y) (threeDigits |> Seq.filter (fun z -> z>=x))) threeDigits
     let largestPalindrome = products |> Seq.filter isPalindrome |> Seq.max
     printfn "#004: %d" largestPalindrome
     assert (largestPalindrome = 906609)

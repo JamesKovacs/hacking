7b6c1152a173a85433728c5dd0afc8b27c821184
Michael L Perry <michael.perry@absg.com>
2011-04-28 12:50:35 -0500

    Turned combo box fix into a behavior.

---
+++ MvvmComboBox/ComboBoxSelectionFixBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace JamesKovacs.MvvmComboBox
+{
+    public class ComboBoxSelectionFixBehavior : Behavior<System.Windows.Controls.ComboBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.SelectionChanged += ComboBox_SelectionChanged;
+        }
+
+        protected override void  OnDetaching()
+        {
+            AssociatedObject.SelectionChanged -= ComboBox_SelectionChanged;
+        }
+
+        void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var binding = AssociatedObject.GetBindingExpression(ComboBox.SelectedItemProperty);
+            if (binding != null)
+            {
+                binding.UpdateTarget();
+            }
+        }
+    }
+}
